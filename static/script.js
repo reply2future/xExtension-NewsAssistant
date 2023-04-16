1d05086f35613a28b6af3804644b00853d15c9c3
mervyn <6359152+reply2future@users.noreply.github.com>
2023-04-16 10:13:31 +0800

    feat(loader): add loader when clicking read button

--- static/script.js
+++ static/script.js
@@ -6,20 +6,35 @@
 	summaryIdsElement.outerHTML = '';
 
 	const setReadBtn = document.querySelector('#set_read_btn');
+	const loader = document.getElementById('loader');
+
+	function showLoading() {
+		loader.style.display = 'flex';
+	}
+
+	function hideLoading() {
+		loader.style.display = 'none';
+	}
 
 	setReadBtn.onclick = function (ev) {
 		ev.stopPropagation();
+
+		showLoading();
 
 		const req = new XMLHttpRequest();
 		req.open('POST', './?c=entry&a=read', true);
 		req.responseType = 'json';
 		req.onerror = function (e) {
+			hideLoading();
+
 			badAjax(this.status == 403);
 		};
 		req.onload = function (e) {
 			if (this.status != 200) {
 				return req.onerror(e);
 			}
+
+			hideLoading();
 
 			window.location.href = '/';
 		};
