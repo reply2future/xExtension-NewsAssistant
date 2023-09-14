57f5b0458a049a6ba02a6610da01784b689b344f
mervyn <6359152+reply2future@users.noreply.github.com>
2023-09-14 16:00:53 +0800

    feat(i18n): support Traditional Chinese
    re #16 #1

---
+++ i18n/zh-tw/ext.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'field' => array(
+		'openai_api_key' => 'OpenAI的API Key',
+		'model' => '模型',
+		'max_tokens' => 'Max Tokens',
+		'temperature' => 'Temperature',
+		'prompt' => 'Prompt',
+		'limit' => '一次性讀取文章數量',
+		'field' => '發送給OpenAI的字段',
+	),
+);
