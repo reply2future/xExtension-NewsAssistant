085a10c3ac4d208c664d4d87bbc865078f873645
mervyn <6359152+reply2future@users.noreply.github.com>
2023-09-14 16:01:40 +0800

    feat(i18n): support Simple Chinese
    re #16 #1

---
+++ i18n/zh-cn/ext.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'field' => array(
+		'openai_api_key' => 'OpenAI的API Key',
+		'model' => '模型',
+		'max_tokens' => 'Max Tokens',
+		'temperature' => 'Temperature',
+		'prompt' => 'Prompt',
+		'limit' => '一次性读取文章数量',
+		'field' => '发送给OpenAI的字段',
+	),
+);
