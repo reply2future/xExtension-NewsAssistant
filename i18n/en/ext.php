6e4dfb52723d8d684aa8e5cedb5508b8138d9f92
mervyn <6359152+reply2future@users.noreply.github.com>
2025-08-26 09:52:49 +0800

    feat(configure): add the unit tip to the field `api_timeout`

--- i18n/en/ext.php
+++ i18n/en/ext.php
@@ -11,7 +11,7 @@ return array(
 		'prompt' => 'Prompt',
 		'limit' => 'Read the number of feeds',
 		'field' => 'Field to send to AI',
-		'api_timeout' => 'API request timeout',
+		'api_timeout' => 'API request timeout (seconds)',
 		'configure_tips' => 'You could input any valid value for `AI Provider` and `Model` even thought they are not in the select list. But make sure they are supported by your AI Provider or [Portkey-AI/gateway](https://portkey.ai/docs/integrations/llms).',
 	),
 );
--- i18n/zh-cn/ext.php
+++ i18n/zh-cn/ext.php
@@ -11,7 +11,7 @@ return array(
 		'prompt' => 'Prompt',
 		'limit' => '一次性读取文章数量',
 		'field' => '发送给AI的字段',
-		'api_timeout' => 'API 请求超时',
+		'api_timeout' => 'API 请求超时(秒)',
 		'configure_tips' => '你可以为 `AI供应商` 和 `模型` 输入任何有效值，即使它们不在下拉列表中。但请确保它们受你的AI供应商或 [Portkey-AI/gateway](https://portkey.ai/docs/integrations/llms) 支持。',
 	),
 );
