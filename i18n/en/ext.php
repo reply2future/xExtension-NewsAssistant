18f04b31eefccfd59ec095da78d3516937c65c60
mervyn <6359152+reply2future@users.noreply.github.com>
2025-08-26 11:52:53 +0800

    feat(configure): support `title,content` fields to summary
    re #47

--- i18n/en/ext.php
+++ i18n/en/ext.php
@@ -10,7 +10,7 @@ return array(
 		'temperature' => 'Temperature',
 		'prompt' => 'Prompt',
 		'limit' => 'Read the number of feeds',
-		'field' => 'Field to send to AI',
+		'fields' => 'Fields to send to AI',
 		'api_timeout' => 'API request timeout (seconds)',
 		'configure_tips' => 'You could input any valid value for `AI Provider` and `Model` even thought they are not in the select list. But make sure they are supported by your AI Provider or [Portkey-AI/gateway](https://portkey.ai/docs/integrations/llms).',
 	),
--- i18n/zh-tw/ext.php
+++ i18n/zh-tw/ext.php
@@ -10,7 +10,7 @@ return array(
 		'temperature' => 'Temperature',
 		'prompt' => 'Prompt',
 		'limit' => '一次性讀取文章數量',
-		'field' => '發送給AI的字段',
+		'fields' => '發送給AI的字段',
 		'api_timeout' => 'API 請求超時(秒)',
 		'configure_tips' => '你可以為 `AI提供者` 和 `模型` 輸入任何有效值，即使它們不在下拉列表中。但請確保它們受你的AI提供者或 [Portkey-AI/gateway](https://portkey.ai/docs/integrations/llms) 支持。',
 	),
