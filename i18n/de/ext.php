e083fec37bb749a6dfb4a07be209cfcfb40c41b0
beautiful-orca <beautiful_orca@proton.me>
2025-08-28 14:41:11 +0200

    German translation

---
+++ i18n/de/ext.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	'field' => array(
+		'openai_base_url' => 'Basis-URL für Anfragen',
+		'openai_api_key' => 'API Schlüssel',
+		'provider' => 'KI Anbieter',
+		'model' => 'Modell',
+		'max_tokens' => 'Maximal verwendete Tokens',
+		'temperature' => 'Temperatur',
+		'prompt' => 'KI Anweisung',
+		'limit' => 'Anzahl eingelesener Feed-Elemente',
+		'field' => 'Inhalt, der an die KI gesendet wird',
+		'api_timeout' => 'Maximale API Anfragedauer',
+	),
+);
