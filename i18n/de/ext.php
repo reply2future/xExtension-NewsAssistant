79634b7a405ec1086d617686d602c9dc4197db63
beautiful-orca <125277611+beautiful-orca@users.noreply.github.com>
2025-08-28 19:56:46 +0200

    fix api_timeout and add missing configure_tips in /de/ext.php

--- i18n/de/ext.php
+++ i18n/de/ext.php
@@ -11,6 +11,7 @@ return array(
 		'prompt' => 'KI Anweisung',
 		'limit' => 'Anzahl eingelesener Feed-Elemente',
 		'field' => 'Inhalt, der an die KI gesendet wird',
-		'api_timeout' => 'Maximale API Anfragedauer',
+		'api_timeout' => 'Maximale API Anfragedauer (Sekunden)',
+		'configure_tips' => 'Für `KI Anbieter` und `Modell` können jegliche gültige Werte eingetragen werden, auch wenn sie nicht in der Auswahlliste auftauchen. Gehe sicher, dass diese für deinen KI Anbieter oder [Portkey-AI/gateway](https://portkey.ai/docs/integrations/llms) gültig sind.',
 	),
 );
